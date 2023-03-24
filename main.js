843a20b0b5214e6e1995dedb74359a7fbe2b3940
alexandrazvereva <alexandrazvereva7@gmail.com>
2023-03-24 14:37:53 -0600

    feat:play prev audio

--- main.js
+++ main.js
@@ -1,26 +1,49 @@
-let isPlaying=false;
-let song1= new Audio("./grieg-morning.mp3");
+let isPlaying = false;
+let song0 = new Audio("./grieg-morning.mp3");
+let song1 = new Audio("./mozart-night-serenade.mp3")
+let song2 = new Audio("./vivaldi-spring.mp3")
+let song3 = new Audio("./tchaikovsky.mp3")
+let play = document.querySelector("#play-song");
+let currentSong = 3;
+const SONGS = [song0, song1, song2, song3];
 
-let play=document.querySelector("#play-song");
-play.addEventListener("click",()=>{
- 
+play.addEventListener("click", () => {
 
-    if(isPlaying === false) {
-    song1.play();
-    play.classList.remove("play-song");
-    play.classList.add("pause-song");
+
+    if (isPlaying === false) {
+        SONGS[currentSong].play();
+        play.classList.remove("play-song");
+        play.classList.add("pause-song");
     } else {
-        song1.pause();
-        play.classList.add("play-song");
+        SONGS[currentSong].pause();
+       play.classList.add("play-song");
         play.classList.remove("pause-song");
     }
-    isPlaying=!isPlaying ;//vozmi isPlay i sdelai protivopoloznoe znachenie
+    isPlaying = !isPlaying;//vozmi isPlay i sdelai protivopoloznoe znachenie
 });
-let prev=document.querySelector("#prev-song");
-prev.addEventListener("click",()=>{
-console.log("You pressed prev-song")
+
+
+let prev = document.querySelector("#prev-song");
+prev.addEventListener("click", () => {
+    console.log("You pressed prev-song")
+    SONGS[currentSong].pause();
+    currentSong--;
+if (currentSong<0){
+    currentSong=SONGS.length-1;
+}
+    SONGS[currentSong].play();
+    isPlaying=true,
+    play.classList.remove("play-song");
+    play.classList.add("pause-song");
+ 
 });
-let next=document.querySelector("#next-song");
-next.addEventListener("click",()=>{
-console.log("You pressed next-song")
+
+
+let next = document.querySelector("#next-song");
+next.addEventListener("click", () => {
+    console.log("You pressed next-song")
+    SONGS[currentSong].pause();
+    currentSong++;
+    SONGS[currentSong].play();
 });
+//dopisat next

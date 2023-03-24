4aef400cc2804dcb59eb25ca95e607b1baaa9be0
alexandrazvereva <alexandrazvereva7@gmail.com>
2023-03-24 14:06:43 -0600

    feat:add play and pause audio

--- main.js
+++ main.js
@@ -1,7 +1,20 @@
-//nazimau na paly, vivodit "vi nazali tuda-to" v consol
+let isPlaying=false;
+let song1= new Audio("./grieg-morning.mp3");
+
 let play=document.querySelector("#play-song");
 play.addEventListener("click",()=>{
-    console.log("You pressed play")
+ 
+
+    if(isPlaying === false) {
+    song1.play();
+    play.classList.remove("play-song");
+    play.classList.add("pause-song");
+    } else {
+        song1.pause();
+        play.classList.add("play-song");
+        play.classList.remove("pause-song");
+    }
+    isPlaying=!isPlaying ;//vozmi isPlay i sdelai protivopoloznoe znachenie
 });
 let prev=document.querySelector("#prev-song");
 prev.addEventListener("click",()=>{

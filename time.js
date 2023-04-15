b42792508fe07eac2e69130ac71792bc4e9d2124
alexandrazvereva <alexandrazvereva7@gmail.com>
2023-04-15 13:33:36 -0600

    fix:impruve clock

--- time.js
+++ time.js
@@ -8,13 +8,16 @@ function showCurrentTime() {
     let minutes=now.getMinutes();
     let seconds=now.getSeconds();
     //console.log(hours +":"+ minutes+":" +seconds+":")
-    hoursDiv.innerHTML=hours<10 ?"0"+hours:hours ;
-    minutesDiv.innerHTML=minutes<10 ?"0"+minutes:minutes ;
-    secondsDiv.innerHTML=seconds <10 ?"0"+seconds:seconds ;
-
+    // hoursDiv.innerHTML=hours<10 ?"0"+hours:hours ;
+    // minutesDiv.innerHTML=minutes<10 ?"0"+minutes:minutes ;
+    // secondsDiv.innerHTML=seconds <10 ?"0"+seconds:seconds ;
+    let totalTime= now.toLocaleTimeString();
+    hoursDiv.innerHTML=totalTime;
     
 }
 setInterval(showCurrentTime,1000);
+
+
 
 
 

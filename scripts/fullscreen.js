9e97bc70cdaa7afc9412437db1e81c70d237da08
alexandrazvereva <alexandrazvereva7@gmail.com>
2023-04-18 15:02:51 -0600

    feat:add fullscreen mode

--- scripts/fullscreen.js
+++ scripts/fullscreen.js
@@ -1,3 +1,13 @@
-document.querySelector(".fullscreen-icon").addEventListener("click", () => {
-    console.log("!")
+const fullScreenIcon = document.querySelector(".fullscreen-icon");
+fullScreenIcon.addEventListener("click", () => {
+    if (!document.fullscreenElement) {
+        document.documentElement.requestFullscreen();
+        fullScreenIcon.classList.add("fullscreen-active");
+        fullScreenIcon.setAttribute("title", "exit fullscreen mode")
+    } else {
+        document.exitFullscreen();
+        fullScreenIcon.classList.remove("fullscreen-active");
+        fullScreenIcon.setAttribute("title", "enter exit fullscreen mode")
+
+    }
 })

26489a2a527c3e3847e64093fd890c007e1e1227
alexandrazvereva <alexandrazvereva7@gmail.com>
2023-04-17 18:52:49 -0600

    add a symbol

--- morning-eveningMessage.js
+++ morning-eveningMessage.js
@@ -4,15 +4,23 @@ function showMessage() {
     let hello;
     let currentHour = new Date().getHours();
     if (currentHour >= 4 && currentHour < 12) // after 4:00AM and before 12:00PM
-    { hello = "Good morning"; }
+    { hello = "Good morning,"; }
     else if (currentHour >= 12 && currentHour <= 18) // after 12:00PM and before 6:00pm
-    { hello = "Good afternoon"; }
+    { hello = "Good afternoon,"; }
     else if (currentHour > 18 || currentHour < 4) // after 6:00pm || before 4:00AM 
     {
-        { hello = "Good evening"; }
+        { hello = "Good evening,"; }
     }
     greetingTag.innerHTML = hello;
 }
 
 setInterval(showMessage, 1000);
+
+let userName = localStorage.getItem("userName");
+userNameInput.value = userName;
+
+userNameInput.addEventListener("input", () => {
+    userName = userNameInput.value;
+    localStorage.setItem("userName", userName);
+})
 

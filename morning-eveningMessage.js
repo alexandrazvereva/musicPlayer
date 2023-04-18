1ba587b7d5c43dc4781e459fb36b201a1b4a9b3a
alexandrazvereva <alexandrazvereva7@gmail.com>
2023-04-17 18:39:32 -0600

    :add time of day info

--- morning-eveningMessage.js
+++ morning-eveningMessage.js
@@ -1,14 +1,18 @@
-const greetingTag=document.
+const greetingTag = document.querySelector(".hello-text");
+const userNameInput = document.querySelector(".userName-input");
 function showMessage() {
+    let hello;
     let currentHour = new Date().getHours();
     if (currentHour >= 4 && currentHour < 12) // after 4:00AM and before 12:00PM
-    { console.log("Good morning") }
+    { hello = "Good morning"; }
     else if (currentHour >= 12 && currentHour <= 18) // after 12:00PM and before 6:00pm
-    { console.log("Good afternoon") }
+    { hello = "Good afternoon"; }
     else if (currentHour > 18 || currentHour < 4) // after 6:00pm || before 4:00AM 
     {
-        { console.log("Good evening") }
+        { hello = "Good evening"; }
     }
+    greetingTag.innerHTML = hello;
 }
-;
-showMessage();
+
+setInterval(showMessage, 1000);
+
